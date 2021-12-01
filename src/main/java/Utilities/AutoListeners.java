6169489231b23a9e4a8bdff1c01d430beb786136
hen <henbenhen@gmail.com>
2021-12-01 17:07:30 +0200

    Update Jenkins V3

--- src/main/java/Utilities/AutoListeners.java
+++ src/main/java/Utilities/AutoListeners.java
@@ -42,8 +42,9 @@ public class AutoListeners implements ITestListener {
 
         System.out.println("---- Test "  + test.getName() + " fail ---");
 
-        if(test.getTestClass().equals("[TestClass name=class TestCases.SeleniumTest]"))
+        if(test.getTestClass().toString().equals("[TestClass name=class TestCases.SeleniumTest]"))
             CommonOps.saveScreenshot();
+
         try {
             MonteScreenRecorder.stopRecord();
         } catch (Exception e) {
