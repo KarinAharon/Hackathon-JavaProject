2308b323e448a5ef4f1493894719aa0b314d4540
karin <krek250@gmail.com>
2021-12-04 11:43:40 +0200

    add descriptions ElectronTest class

--- src/test/java/TestCases/ElectronTest.java
+++ src/test/java/TestCases/ElectronTest.java
@@ -11,20 +11,22 @@ import static WorkFlows.ElectronWF.DeleteTaskByName.deleteTaskByName;
 
 public class ElectronTest extends CommonOps {
 
-    @Test
+    @Test(description = "create new task")
     public void test01_createNewTask(){
         Assert.assertTrue(createTask("Do Home Work", "red"));
-        Assert.assertTrue(createTask("Do Home Work2", "blue"));
-        Assert.assertTrue(createTask("Do Home Work3", "green"));
+        Assert.assertTrue(createTask("Make food", "blue"));
+        Assert.assertTrue(createTask("Wash dishes", "green"));
     }
 
-    @Test
+    @Test(description = "delete task by name")
+    public void test03_delTaskByName(){
+        Assert.assertTrue(deleteTaskByName("Make food"));
+    }
+
+    @Test(description = "delete all tasks")
     public void test02_delAllTask(){
         Assert.assertTrue(deleteAllTask());
     }
 
-    @Test
-    public void test03_delTaskByName(){
-        Assert.assertTrue(deleteTaskByName("Do Home Work2"));
-    }
+
 }
